c754d7f77b96bdf718b95fe4b92dab6b1165dcb8
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2023-11-29 23:50:52 +0000

    (🔴) feat: add case for number two

--- src/katas/fizzbuzz/fizzbuzz.test.ts
+++ src/katas/fizzbuzz/fizzbuzz.test.ts
@@ -4,4 +4,7 @@ describe('FizzBuzz', () => {
   it('should return 1 as string when given 1 number', () => {
     expect(fizzbuzz(1)).toEqual('1');
   })
+  it('should return 2 as string when given 2 number', () => {
+    expect(fizzbuzz(2)).toEqual('2');
+  })
 })
