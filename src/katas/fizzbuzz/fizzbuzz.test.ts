2acf6530dd86716f4acf9ad75c759d7077fda5a1
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2023-11-29 23:56:08 +0000

    (🔴) feat: add case for number five

--- src/katas/fizzbuzz/fizzbuzz.test.ts
+++ src/katas/fizzbuzz/fizzbuzz.test.ts
@@ -1,13 +1,16 @@
 import {fizzbuzz} from "./fizzbuzz";
 
 describe('FizzBuzz', () => {
-  it('should return 1 as string when given number 1', () => {
+  it('should return 1 as string for given number 1', () => {
     expect(fizzbuzz(1)).toEqual('1');
   })
-  it('should return 2 as string when given number 2', () => {
+  it('should return 2 as string for given number 2', () => {
     expect(fizzbuzz(2)).toEqual('2');
   })
-    it('should return fizz when given number 3', () => {
-        expect(fizzbuzz(3)).toEqual('fizz');
-    })
+  it('should return fizz for given number 3', () => {
+    expect(fizzbuzz(3)).toEqual('fizz');
+  })
+  it('should return buzz for given number 5', () => {
+    expect(fizzbuzz(5)).toEqual('buzz');
+  })
 })
