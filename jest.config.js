6eb52e491d4d3ae824824d550b700e118e8b76af
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2023-11-29 23:15:39 +0000

    chore: add jest coverage configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  verbose: true,
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    verbose: true,
+    collectCoverage: true,
+    coverageDirectory: './coverage',
 };
