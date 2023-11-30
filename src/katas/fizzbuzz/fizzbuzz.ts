20456b9d706c8ff505b25a8084f943282c2f1e81
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2023-11-30 00:08:21 +0000

    (🟢) feat: add case for any number divisible by five

--- src/katas/fizzbuzz/fizzbuzz.ts
+++ src/katas/fizzbuzz/fizzbuzz.ts
@@ -2,7 +2,7 @@ export function fizzbuzz(number: number): string {
     if (number === 15) {
         return 'fizzbuzz'
     }
-    if (number === 5) {
+    if (number % 5 === 0) {
         return 'buzz'
     }
     if (number % 3 === 0) {
