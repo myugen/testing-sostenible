5f6935dcb29df3bd72edf52d57a6ca388ab42e4b
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2023-11-30 00:19:12 +0000

    refactor: abstract all divisible evaluations to one method

--- src/katas/fizzbuzz/fizzbuzz.ts
+++ src/katas/fizzbuzz/fizzbuzz.ts
@@ -1,11 +1,15 @@
 export function fizzbuzz(number: number): string {
-    if (number % 15 === 0) {
+    function isDivisibleBy(divisor: number) {
+        return number % divisor === 0;
+    }
+
+    if (isDivisibleBy(15)) {
         return 'fizzbuzz'
     }
-    if (number % 5 === 0) {
+    if (isDivisibleBy(5)) {
         return 'buzz'
     }
-    if (number % 3 === 0) {
+    if (isDivisibleBy(3)) {
         return 'fizz'
     }
     return number.toString()
